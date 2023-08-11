9c1f682883d96f98bc40b6794a83bb24457c6f9f
edsonpetrinijr <edsonpetrinijr@gmail.com>
2023-08-11 10:54:33 -0300

    Criando classe 'Todo'

--- todolist/src/main/java/br/com/todolist/TodolistApplication.java
+++ todolist/src/main/java/br/com/todolist/TodolistApplication.java
@@ -2,12 +2,16 @@ package br.com.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
+@RestController
 public class TodolistApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TodolistApplication.class, args);
 	}
-
+	@GetMapping("/")
+	public String hello() {
+		return String.format("Hello World!");
+	}
 }

5eff66b24caa0e836198babdda282f0603941c12
edsonpetrinijr <edsonpetrinijr@gmail.com>
2023-08-24 17:08:16 -0300

    Adição dos endpoints 'PUT'

--- todolist/src/main/java/br/com/todolist/TodoController.java
+++ todolist/src/main/java/br/com/todolist/TodoController.java
@@ -1,5 +1,6 @@
 package br.com.todolist;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -37,6 +38,47 @@ public class TodoController {
             this.todoRepository.deleteById(todoId);
 
             return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/{todoId}/start_task")
+    public ResponseEntity<Todo> startTask(@PathVariable Integer todoId) {
+        Todo todoDatabase = this.todoRepository.findById(todoId).get();
+
+        if (todoDatabase != null) {
+            todoDatabase.setStatus(StatusEnum.IN_PROGRESS);
+            this.todoRepository.save(todoDatabase);
+            return ResponseEntity.ok(todoDatabase);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/{todoId}/end_task")
+    public ResponseEntity<Todo> endTask(@PathVariable Integer todoId) {
+        Todo todoDatabase = this.todoRepository.findById(todoId).get();
+
+        if (todoDatabase != null) {
+            todoDatabase.setStatus(StatusEnum.FINISHED);
+            this.todoRepository.save(todoDatabase);
+            return ResponseEntity.ok(todoDatabase);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/{todoId}")
+    public ResponseEntity<Todo> update(@PathVariable Integer todoId, @RequestBody Todo todo) {
+        Todo todoDatabase = this.todoRepository.findById(todoId).get();
+
+        if (todoDatabase != null) {
+            BeanUtils.copyProperties(todo, todoDatabase, "id");
+
+            Todo updatedTodo = this.todoRepository.save(todoDatabase);
+
+            return ResponseEntity.ok(updatedTodo);
         } else {
             return ResponseEntity.notFound().build();
         }
